a5ebcaed8d4d68cc8462be85a37ced79c9befc4e
Rex4you <123090271+Rex4you@users.noreply.github.com>
2023-02-25 17:00:15 -0600

    Update magicNumbers.kt

--- magicNumbers.kt
+++ magicNumbers.kt
@@ -1,24 +1,35 @@
-CODE
+fun main() {
+    println("Enter month")
 
-Month = int(input("Enter a month (in numeric form) :"))         #Asking the user to enter a month.
-Day =  int(input("Enter a day :"))                              #Asking the user to enter a day.
-Year = int(input("Enter a two-digit year:"))                    #Asking the user to enter a two-digit year.
-Determine = Month * Day ==Year                                 #determine whether the month times the day equals the year.
-if Determine:                              #determine whether the month times the day equals the year and check.                                                   
-    print(" The date is magic.")                                #if it is true display message.
-else:                                                                                                  
-    print("The date is not magic.")                             #else display another message.
+    val month = readln().toInt()
 
-OUTPUT
+    if (month > 12 || month < 1) {
+        println("error, number is not logic");
+        return;
+    }
 
-Enter a month (in numeric form) :6
-Enter a day :10
-Enter a two-digit year:60
- The date is magic.
+    println("Enter a day")
+    val day = readln().toInt()
+
+    if (day > 31 || day < 1) {
+        println("error, number is not logic");
+        return;
+    }
+
+    println("Enter a year")
+    val year = readln().toInt()
+    if (year < 1) {
+        println("error, number is not logic")
+    }
+
+    val multiplication = month * day
+    if( multiplication ==year) {
+        println("date is magic")
+    }
+    else
+            println("date is not magic")
 
 
+}
 
-Enter a month (in numeric form) :3
-Enter a day :4
-Enter a two-digit year:14
-The date is not magic.
+main()
