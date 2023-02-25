d50a499b0bc35a11783587bc56803158f0f05e6e
Rex4you <123090271+Rex4you@users.noreply.github.com>
2023-02-25 17:13:55 -0600

    Update bodyMass.kt

--- bodyMass.kt
+++ bodyMass.kt
@@ -1,48 +1,29 @@
-// Variables
-double weight; // The user's weight
-double height; // The user's height
-double bmi; // The user's BMI
+fun main() {
+    println("Welcome to the BMI Calculator!")
+    println("Please enter your weight in pounds: ")
+    val weight = readLine()?.toDoubleOrNull()
 
-// Create a Scanner object for keyboard input.
-Scanner keyboard = new Scanner(System.in);
+    println("Please enter your height in inches: ")
+    val height = readLine()?.toDoubleOrNull()
 
-// Tell the user what the program will do.
-System.out.println("This program will calculate your body mass index, or BMI.");
 
-// Get the user's weight.
-System.out.print("Enter your weight, in pounds: ");
-weight = keyboard.nextDouble();
+    if (weight != null && height != null && weight > 0 && height > 0) {
+        val bmi = (weight * 703) / (height * height)
 
-// Get the user's height.
-System.out.print("Enter your height, in inches: ");
-height = keyboard.nextDouble();
-
-static double calculateBMI (double height, double weight) {
-    return weight * 703 / (height * height);
-}
-
-static String bmiDescription (double bmi) {
-    if (bmi < 18.5) {
-        return "You are underweight.";
-    } else {
-        if (bmi > 25) {
-            return "You are overweight.";
+        val weightCategory = if (bmi < 18.5) {
+            "underweight"
+        } else if (bmi < 25) {
+            "optimal weight"
         } else {
-            return "Your weight is optimal.";
+            "overweight"
         }
+
+        println("Your BMI is: $bmi")
+        println("You are $weightCategory")
+    } else {
+        println("Invalid input. Please enter valid positive numbers.")
     }
+
 }
 
-// Calculate the user's body mass index.
-bmi =  calculateBMI(height, weight);
-
-// Display the user's BMI.
-System.out.println("Your body mass index (BMI) is " + bmi);
-
-System.out.println(bmiDescription(bmi));
-
-This program will calculate your body mass index, or BMI.
-Enter your weight, in pounds: 220
-Enter your height, in inches: 68
-Your body mass index (BMI) is 33.44723183391003
-You are overweight.
+main()
